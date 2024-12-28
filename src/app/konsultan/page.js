6a5d2eca823e2b9a4d2d5f1786cfebc614a6d8f1
fanuhi <fadhlinurhimawan@gmail.com>
2024-12-28 11:24:03 +0700

    reform portofolio section

--- src/app/konsultan/page.js
+++ src/app/konsultan/page.js
@@ -6,30 +6,12 @@ import Image from "next/image";
 import { FaWhatsapp, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { HiChevronLeft, HiChevronRight  } from "react-icons/hi";
 import { AiOutlineWhatsApp } from "react-icons/ai";
-import Content from "./Content.json";
-import LandingPage1 from "./sections/landing_page";
+import Content from "../Content.json";
+import {LandingPage2} from "../sections/landing_page";
 
 export default function Home() {
 
-  const contentPerPageInitial = window.innerWidth <= 500 ? 1 : 2;
-
-  const portofolios = Content.Portofolio
-  const [currentPage, setCurrentPage] = useState(1);
-  const [contentPerPage, setContentPerPage] = useState(contentPerPageInitial);
-  const portoliosLength = portofolios.length
-  const totalPortosPages = portoliosLength / contentPerPage
-
-  const indexOfLastPost = currentPage * contentPerPage;
-  const indexOfFirstPost = indexOfLastPost - contentPerPage;
-  const currentPortofolios = portofolios.slice(indexOfFirstPost, indexOfLastPost);
-
   const scrollWrapperRef = useRef(null);
-
-  const handlePorto = (page) => {
-    if (page <= 1) {setCurrentPage(1)}
-    else if (page >= totalPortosPages) {setCurrentPage(totalPortosPages)}
-    else {setCurrentPage(page)}     
-  }
 
   const scroll = (direction) => {
     if (scrollWrapperRef.current) {
@@ -50,7 +32,7 @@ export default function Home() {
       }
   };
 
-  const data = Content.Mission;
+  const data = Content.CompanyFeatures;
   const [activeIndex, setActiveIndex] = useState(1);
 
   const handleLeftClick = () => {
@@ -66,77 +48,8 @@ export default function Home() {
   
       <div className='w-full max-w-[2024px] lg:m-auto xs:m-0 grid grid-cols-1 lg:gap-20 xs:gap-0 place-content-center place-items-center'>
 
-        <LandingPage1 scrollToTarget={scrollToTarget} />
+        <LandingPage2 scrollToTarget={scrollToTarget} />
 
-        {/** CONTINUESNESS LINE */}
-        <div ref={targetRef} className='lg:w-5/6 xs:w-full lg:p-20 xs:p-5 lg:-mb-40 xs:-mb-10 relative lg:border lg:border-b-0 rounded-[61px] xs:border-collapse border-green-3'>
-
-          {/** GET TO KNOW SECTION */}
-          <div className='relative flex flex-col lg:gap-10 xs:gap-2 justify-center items-center'>
-            {/** TITTLE SECTION */}
-            <div className='lg:w-1/2 xs:w-full flex flex-col gap-2 justify-center lg:text-center xs:text-start'>
-              <h6 className='lg:block xs:hidden lg:text-[14px] xs:text-[10px] font-bold text-green-3'>Kami membidik dunia dengan martabat dan ketekunan. Tidak ada hal yang dapat mematahkan semangat kami dalam hal mengembangkan keinginan klien kami.</h6>
-              <h2 className='lg:text-[32px] xs:text-[40px] font-bold lg:text-center xs:text-start'>Kenali Misi Kami</h2>            
-            </div>
-            {/** VISION MISSION DIV SENTION */}
-            <div className='lg:w-3/4 xs:w-full lg:p-10 xs:p-5 flex flex-col gap-5 rounded-[17px] bg-radial-gray'>
-              <div className='flex lg:flex-row xs:flex-col lg:gap-14 xs:gap-5 lg:text-[13px] xs:text-[10px] font-bold text-justify leading-loose'>
-                <p>Menjadi perusahaan multidisiplin yang unggul dalam memberikan solusi inovatif, berkelanjutan, dan terpercaya di bidang konsultansi, telekomunikasi, agribisnis, jasa konstruksi, dan perdagangan bebas untuk mendukung pembangunan yang inklusif dan berdaya saing tinggi.</p>                
-              </div>
-              <button className='w-full py-2 lg:text-[24px] xs:text-[18px] font-bold rounded-[15px] bg-green-3 text-gray-300'>Our Works</button>
-            </div>
-            {/** BG SHADOW */}
-            <div className='w-2/4 h-10 p-2 absolute bottom-0 -z-10 box-shadow-1'></div>
-          </div>  
-        
-        </div>      
-
-        {/** GREEN BG SECTION */}
-        <div className='w-full -mt-16 pb-10 pt-32 flex flex-col justify-center items-center bg-green-3 overflow-hidden'>
-          
-          {/** FEATURES SECTION */}
-          <div className='lg:w-4/6 xs:w-10/12 lg:grid lg:grid-cols-2 xs:flex xs:flex-col xs:items-center lg:gap-36 xs:gap-14'>
-            {/** left section */}
-            <div className='w-full flex flex-col lg:gap-10 xs:gap-2 justify-evenly text-left text-white-2'>
-              <div>
-                <p className='text-[20px] font-normal'>Misi</p>
-                <h1 className='text-[38px] font-black leading-tight'>Kenali bagaimana kami mewujudkan misi kami</h1>
-              </div>              
-              <p className='lg:text-[16px] xs:text-[14px] font-normal text-justify'>Mewujudkan tujuan yang besar memerlukan serangkaian strategi dan disiplin yang kuat. Arakia berkomitmen menjaga nilai-nilai berikut untuk mencapai tujuan itu.</p>
-            </div>
-            {/** right section */}
-            <div className='w-full h-80 relative flex justify-center items-center'>
-            {Content.Mission.map((item, index) =>  {
-
-                const offset = index - activeIndex;
-                const scale = 1 - Math.min(Math.abs(offset) * 0.1, 0.5); // Decrease scale for non-active cards
-                const zIndex = data.length - Math.abs(offset); // Ensure the active card stays on top         
-                
-                return (
-                <div key={index} 
-                className={"lg:w-[19rem] xs:w-[15rem] h-[350px] py-5 lg:px-10 xs:px-7 absolute flex flex-col justify-between gap-5 rounded-[21px] box-shadow-2 transition-all duration-500 ease-in-out bg-green-4"}
-                style={{
-                  transform: `translateX(${offset * 40}px) scale(${scale})`,
-                  zIndex: zIndex,
-                  opacity: scale, // Reduce opacity slightly for non-active cards
-                }}>
-                  <div className=''>
-                    <p className='text-[14px] font-normal'>Features</p>
-                    <h1 className='text-[28px] font-black'>{item.title}</h1>
-                  </div>                  
-                  <div className='place-content-center'><p className='text-[14px] font-normal text-justify'>{item.desc}</p></div>
-                  <div className='flex gap-3'>
-                    <button className='p-2 rounded-full text-white bg-green-3' onClick={handleRightClick}><HiChevronLeft className='size-6' /></button>
-                    <button className='p-2 rounded-full text-white bg-green-3' onClick={handleLeftClick}><HiChevronRight className='size-6' /></button>
-                  </div>
-                </div>      
-              
-              );})}
-              </div>
-
-          </div>
-
-        </div>     
 
         {/** CONTINUESNESS LINE AND BACKGROUND */}     
         <div className='lg:w-11/12 xs:w-full lg:p-20 xs:p-5 relative flex flex-col lg:gap-20 xs:gap-12 justify-center items-center overflow-hidden border lg:rounded-[55px] xs:rounded-none lg:border-green-3 xs:border-none'>
@@ -173,25 +86,27 @@ export default function Home() {
           <div className='lg:w-11/12 xs:w-full relative flex flex-col lg:gap-5 xs:gap-0'>
             {/** Top title */}
             <div className="flex justify-center items-center">
-              <button className="p-2 rounded-full bg-green-1"><HiChevronLeft size={25} onClick={() => handlePorto(currentPage - 1)} /></button>
+              <button className="p-2 rounded-full bg-green-1"><HiChevronLeft size={25} /></button>
               <div className='lg:px-20 xs:px-5 text-center'>
                 <p className='lg:text-[24px] xs:text-[12px] font-semibold text-green-3'>Lihat rekam jejak kami</p>
                 <h2 className='lg:text-[40px] xs:text-[20px] font-black'>PORTOFOLIO</h2>
               </div>
-              <button className="p-2 rounded-full bg-green-1"><HiChevronRight size={25} onClick={() => handlePorto(currentPage + 1)} /></button>
+              <button className="p-2 rounded-full bg-green-1"><HiChevronRight size={25} /></button>
             </div>
             {/** card section */}
             <div className='w-full flex flex-row gap-12'>
-              {currentPortofolios.map((item,index) => (
-                <div key={index} className='lg:basis-[50%] xs:basis-0-[100%] flex-shrink-0 xs:w-full lg:p-10 xs:p-5 relative lg:rounded-[46px] xs:rounded-[28px] box-shadow-4 bg-white'>
-                  <div className='lg:p-20 xs:p-12 text-white rounded-[23px] bg-green-3 text-center'>Pict</div>
-                  <h1 className='mt-5 text-[32px] font-black'>{item.title}</h1>
-                  <p className='lg:text-[16px] xs:text-[12px] font-normal text-justify leading-tight'>{item.desc}</p>
-                  <h1 className='absolute bottom-0 right-8 lg:text-[96px] xs:text-[64px] font-black text-green-3-half'>#1</h1>
-                </div> 
-              ))
-
-              }             
+              <div className='lg:basis-[50%] flex-shrink-0 xs:w-full lg:p-10 xs:p-5 relative lg:rounded-[46px] xs:rounded-[28px] box-shadow-4 bg-white'>
+                <div className='lg:p-20 xs:p-12 text-white rounded-[23px] bg-green-3 text-center'>Pict</div>
+                <h1 className='mt-5 text-[32px] font-black'>Nama Portofolio</h1>
+                <p className='lg:text-[16px] xs:text-[12px] font-normal text-justify leading-tight'>To create a sustainable and equitable future for underdeveloped areas, ARAKIA is committed to creating opportunities for the community through education, technology,. mr robot mus be one of the best series i have ever watched</p>
+                <h1 className='absolute bottom-0 right-8 lg:text-[96px] xs:text-[64px] font-black text-green-3-half'>#1</h1>
+              </div>
+              <div className='lg:basis-[50%] flex-shrink-0 xs:w-full lg:p-10 xs:p-5 relative lg:rounded-[46px] xs:rounded-[28px] box-shadow-4 bg-white'>
+                <div className='lg:p-20 xs:p-12 text-white rounded-[23px] bg-green-3 text-center'>Pict</div>
+                <h1 className='mt-5 text-[32px] font-black'>Nama Portofolio</h1>
+                <p className='lg:text-[16px] xs:text-[12px] font-normal text-justify leading-tight'>To create a sustainable and equitable future for underdeveloped areas, ARAKIA is committed to creating opportunities for the community through education, technology,. mr robot mus be one of the best series i have ever watched</p>
+                <h1 className='absolute bottom-0 right-8 lg:text-[96px] xs:text-[64px] font-black text-green-3-half'>#1</h1>
+              </div>
             </div>
             {/** PAGINATION SECTION */}
             <div className='w-full mt-5 flex gap-5 items-center justify-center'>
--- src/app/sections/landing_page.js
+++ src/app/sections/landing_page.js
@@ -1,14 +1,20 @@
-import { useRef } from "react";
+'use client';
 
-import Button1 from "../components/buttons";
+import { useRouter } from "next/navigation";
+
+import { Navbar1 } from "../components/navbars";
 import Content from "../Content.json";
 
 import { FaCheck } from "react-icons/fa6";
 import { FaRegCircle } from "react-icons/fa";
+import { GiLindenLeaf } from "react-icons/gi";
 import Image from "next/image";
-import { Navbar1 } from "../components/navbars";
+
 
 export const LandingPage1 = ({scrollToTarget}) => {
+
+    const router = useRouter();
+
     return (
         <>
 
@@ -27,7 +33,7 @@ export const LandingPage1 = ({scrollToTarget}) => {
                         <p className='lg:text-[40px] xs:text-[24px] lg:font-light xs:font-light text-white lg:text-center xs:text-end leading-tight'>Wujudkan Kebutuhan Anda dengan Layanan Kami: </p>
                         {/** FLYING BUTTON */}
                         <div className=" lg:hidden xs:flex lg:text-[16px] xs:text-[12px] lg:gap-10 xs:gap-2">
-                            <button className="lg:px-6 lg:py-3 xs:px-3 xs:py-2 font-black text-green-3 rounded-full bg-white" onClick={() => window.open("/konsultan")}>KONSULTAN</button>
+                        <button className="lg:px-6 lg:py-3 xs:px-3 xs:py-2 font-black text-green-3 rounded-full bg-white" onClick={() => router.push("/konsultan")}>KONSULTAN</button>
                             <button className="lg:px-6 lg:py-3 xs:px-3 xs:py-2 font-black text-green-3 rounded-full bg-green-4">KONTAK</button>
                         </div>
 
@@ -36,7 +42,7 @@ export const LandingPage1 = ({scrollToTarget}) => {
                     </div>
 
                     {/** BOTTOM SECTION */}
-                    <div className="w-full h-full p-2 flex flex-col gap-3 justify-start items-center lg:bg-transparent xs:bg-white">
+                    <div className="w-full h-full px-6 py-2 flex flex-col gap-3 justify-start items-center lg:bg-transparent xs:bg-white">
                         <h1 className="lg:hidden xs:block font-black text-green-3">Our Approach</h1>
                         <div className="lg:w-auto xs:w-full px-8 py-2 flex lg:flex-row xs:flex-col lg:gap-3 xs:gap-1 lg:items-end xs:items-start rounded-lg lg:border-none xs:border border-green-3 bg-white">
                             <h4 className="text-[16px] font-bold">Bersama Arakia:</h4>
@@ -58,7 +64,7 @@ export const LandingPage1 = ({scrollToTarget}) => {
 
                 {/** FLYING BUTTON */}
                 <div className="mt-10 lg:flex xs:hidden gap-10">
-                    <button className="px-6 py-3 text-[16px] font-black text-green-3 rounded-full bg-white" onClick={() => window.open("/konsultan")}>KONSULTAN</button>
+                <button className="lg:px-6 lg:py-3 xs:px-3 xs:py-2 font-black text-green-3 rounded-full bg-white" onClick={() => router.push("/konsultan")}>KONSULTAN</button>
                     <button className="px-6 py-3 text-[16px] font-black text-green-3 rounded-full bg-green-4">KONTAK</button>
                 </div>
 
@@ -74,55 +80,68 @@ export const LandingPage1 = ({scrollToTarget}) => {
     );
 };
 
-export const LandingPage2 = () => {
+export const LandingPage2 = ({scrollToTarget}) => {
+
+    const router = useRouter();
+    
     return (
         <>
 
         {/** TOP COVER */}
-        <div className='xs:mt-0 lg:w-full xs:w-full h-auto relative flex justify-start items-start overflow-hidden'>
+        <div className='xs:mt-0 lg:w-full xs:w-full lg:h-auto xs:h-screen relative flex justify-start items-start overflow-hidden'>
 
             {/** CONTENT CONTAINER */}
-            <div className='w-full h-full py-5 lg:px-10 xs:px-2 flex flex-col justify-between top-0 place-items-center'>
-                {/** NAVBAR */}
-            <div className='w-full flex  justify-between self-start text-[14px] font-semibold text-white'>
-                <div className='flex justify-center items-center gap-5'>
-                    <Image className="object-cover rounded-[39px]" src={"/arakia/images/LOGO-COMPACT.png"} width={24} height={24} alt="" />  
-                    <h4>Amanah Citra Cendaka</h4>
+            <div className='w-full h-full lg:py-5 xs:py-0 lg:px-0 xs:px-0 flex flex-col gap-8 justify-between top-0 items-center'>                
+                <Navbar1 />
+
+                {/** DESC CONTENT */}
+                <div className='lg:w-2/4 lg:mt-20 xs:mt-0 xs:w-full h-full flex flex-col gap-2 justify-center items-center'>
+
+                    {/** TOP SECTION */}
+                    <div className="lg:px-0 xs:px-5 lg:py-0 py-20 relative flex flex-col gap-4 xs:justify-between items-end overflow-hidden">
+                        <p className='lg:text-[40px] xs:text-[18px] lg:font-light xs:font-light text-white lg:text-center xs:text-end leading-tight'>ACC membantu anda dalam pembuatan Konstruksi bangunan, Pengembangan website dan lain - lain.
+                        </p>
+                        {/** FLYING BUTTON */}
+                        <div className=" lg:hidden xs:flex lg:text-[16px] xs:text-[12px] lg:gap-10 xs:gap-2">
+                            <button className="lg:px-6 lg:py-3 xs:px-3 xs:py-2 font-black text-green-3 rounded-full bg-white" onClick={() => router.back()}>BACK</button>
+                            <button className="lg:px-6 lg:py-3 xs:px-3 xs:py-2 font-black text-green-3 rounded-full bg-green-4">KONTAK</button>
+                        </div>
+
+                        <div className="w-full h-full absolute left-0 top-0 lg:hidden xs:block -z-10 bg-cover"></div>
+                        <img className="w-full h-full lg:hidden xs:block object-cover absolute left-0 top-0 -z-20" src={"/arakia/images/bg-field.jpg"} alt="landing page background" />
+                    </div>
+
+                    {/** BOTTOM SECTION */}
+                    <div className="w-full h-full px-6 py-2 flex flex-col gap-2 justify-start items-center lg:bg-transparent xs:bg-white">
+                        <h1 className="lg:hidden xs:block font-black text-green-3">Our Approach</h1>
+                        {Content.CompanyFeatures.map((item,index) => (
+                            <button key={index} className="px-3 py-1 flex flex-row gap-5 items-center xs:border rounded-md border-green-3">
+                                <GiLindenLeaf className="size-10" />
+                                {item.icon}
+                                <div className="w-3/4 flex flex-col xs:items-start xs:text-start">
+                                    <h3 className="xs:text-[12px] font-bold">{item.title}</h3>
+                                    <p className="xs:text-[9px] font-normal text-justify">{item.desc}</p>
+                                </div>
+                            </button>
+                        ))
+
+                        }
+                        <button className="w-full p-2 lg:hidden xs:block rounded-xl text-white bg-green-3" onClick={scrollToTarget}>Scroll Down</button>
+                    </div>
                 </div>
 
-                <div className='lg:flex xs:hidden gap-10'>
-                <button>Home</button>
-                <button>About Us</button>
-                <button>Our Works</button>
-                <button>Contact Us</button>
-                <button className='px-4 py-2 rounded-md bg-green-500'>Donate</button>
+                {/** FLYING BUTTON */}
+                <div className="mt-10 lg:flex xs:hidden gap-10">
+                    <button className="px-6 py-3 text-[16px] font-black text-green-3 rounded-full bg-white" onClick={() => window.open("/konsultan")}>KONSULTAN</button>
+                    <button className="px-6 py-3 text-[16px] font-black text-green-3 rounded-full bg-green-4">KONTAK</button>
                 </div>
-            </div>
-
-            {/** TRY FOR FREE */}
-            <div className='lg:w-1/3 xs:w-3/4 h-full mt-16 top-1/2 flex flex-col gap-4 justify-center items-center text-[14px]'>
-                <p className='font-bold text-white text-center'>ACC membantu anda dalam pembuatan konstruksi bangunan, pengembangan website dan lain lain</p>
-                <button className='lg:w-1/3 xs:w-full py-2 font-black text-black rounded-[9px] bg-gray-200'>Try for free</button>
-            </div>
-
-            {/** APPROACH SECTION */}
-            <div className='w-full h-full lg:px-20 xs:px-0 mt-20 flex flex-col gap-5 items-center'>
-                <h2 className='text-[24px] font-black text-gray-200'>Misi Kami</h2>
-                <div className='w-full grid lg:grid-cols-4 xs:grid-cols-2 lg:gap-5 xs:gap-2'>
-                
-                {Content.Mission.map((item, index) => (
-                    <Button1 title={item.title} desc={item.desc} key={index} />
-                ))
-
-                }
-                </div>
-            </div>
 
             </div>
 
             {/** BACKGROUND IMAGE */}
-            <div className="w-full h-full absolute -z-10 bg-cover"></div>
-            <Image className="object-cover absolute -z-20" src={"/arakia/images/bg-field.jpg"} layout="fill" alt="landing page background" />
+            <div className="w-full h-full absolute lg:block xs:hidden -z-10 bg-cover"></div>            
+            <Image className="lg:block xs:hidden object-cover absolute -z-30 " src={"/arakia/images/bg-field.jpg"} layout="fill" alt="landing page background" />         
+
             </div>
 
         </>
