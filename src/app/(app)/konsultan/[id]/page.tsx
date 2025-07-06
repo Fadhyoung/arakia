79988f90c0fe591746dccd2e3d9a0cdff32d8f10
Fadhli Nur Himawan <fadhlinurhimawan@gmail.com>
2025-07-06 17:08:20 +0700

    develop: fix pagePorps bug

--- src/app/(app)/konsultan/[id]/page.tsx
+++ src/app/(app)/konsultan/[id]/page.tsx
@@ -4,14 +4,9 @@ import Content from "@/app/Content.json";
 
 export { generateStaticParams };
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default function DetailConsultanPage({ params }: PageProps) {
-  const consultant = Content.Consultations.find((item) => item.path === params.id);
+export default async function DetailConsultanPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const consultant = Content.Consultations.find((item) => item.path === id);
 
   if (!consultant) {
     return <div>Consultant not found</div>;
